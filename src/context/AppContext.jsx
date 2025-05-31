c9311b66211f2e9dc873380566a07562765a74ce
Ashan Lokuge <ashanlokuge10@gmail.com>
2025-05-31 16:46:31 +0530

    best sellers component done

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,17 +1,70 @@
-import { Children, createContext,useContext, useState } from "react";
+import { Children, createContext,useContext, useEffect, useState } from "react";
 
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({children})=>{
 
+  const currency = import.meta.VITE_CURRENCY;
+
   const navigate = useNavigate();
   const [user,setUser] = useState(true)
   const [isSeller, setIsSeller] = useState(false)
   const [showUserLogin, setShowUserLogin] = useState(false)
-  
-  const value = {navigate,user, setUser,setIsSeller,isSeller,showUserLogin, setShowUserLogin}
+  const [products,setProducts] =useState([])  
+
+  const [cartItems,setCartItems] =useState({})  
+
+  const fetchProducts =async ()=>{
+    setProducts(dummyProducts)
+  }
+
+  //add products to cart
+  const addToCart = (itemId)=>{
+    let cartData =structuredClone(cartItems);
+
+    if (cartItems[itemId]){
+      cartData[itemId] += 1;
+    }else{
+      cartData[itemId] =1;
+    }
+
+    setCartItems(cartData);
+    toast.success("Added to Cart")
+  }
+
+  //update cart Item Quantity
+
+  const updateCartItem = (itemId,quantity)=>{
+    let cartData = structuredClone(cartItems);
+    cartData[itemId] =quantity;
+    setCartItems(cartData)
+    toast.success("Cart Updated")
+  }
+
+  //remove products from cart
+
+  const removeFromCart = (itemId)=>{
+      let cartData = structuredClone(cartItems);
+      if(cartData[itemId]){
+        cartData[itemId] -= 1;
+        if(cartData[itemId]===0){
+          delete cartData[itemId];
+        }
+      }
+      toast.success("Removed from cart")
+      setCartItems(cartData)
+  }
+
+  useEffect(()=>{
+    fetchProducts()
+  },[])
+
+
+  const value = {navigate,user, setUser,setIsSeller,isSeller,showUserLogin, setShowUserLogin, products,currency,addToCart,updateCartItem,removeFromCart, cartItems}
 
   return <AppContext.Provider value={value}>
     {children}
